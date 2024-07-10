4c8f33c1a9f6142df2c6e434eecce3a3b912e3a1
mdashikurrahman581 <mdashikurrahman581@gmail.com>
2024-07-10 03:04:10 +0200

    AWT2021

--- Final_Lab_task1/src/Components/ProductDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const ProductDetails=()=>{   
-    return(
-        <div>    
-            <h1>This is product details</h1>
-        </div>
-    )
-}
-export default ProductDetails;
